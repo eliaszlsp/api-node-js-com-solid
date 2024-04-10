d72ed8127184bbe2f5ee81bdea667775f8e2872e
eliaszlsp <estudoselias@hotmail.com>
2024-04-10 06:09:26 -0300

    prisma:install🫡

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  semi: true, // Adicionar ponto e vírgula no final das linhas
+  semi: false, // Adicionar ponto e vírgula no final das linhas
   singleQuote: true, // Usar aspas simples
   trailingComma: 'all', // Adicionar vírgula após o último item em objetos e arrays
 };
